d15abcbed53b7f929e6e441313b113eb2866c6c7
Ramil Mirhasanov <ramil600@yahoo.com>
2021-10-30 20:47:35 +0300

    Add graceful shutdown of dispatcher

--- main.go
+++ main.go
@@ -4,16 +4,28 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/ramil600/sensors/config"
 	"github.com/ramil600/sensors/rabbit"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+
 
 	cfg := config.NewConfig()
 	dispatcher := rabbit.NewDispatcher(cfg.PublishDSN)
 
 	dispatcher.Subscribe(cfg.EventsQueue)
-	dispatcher.Shutdown()
 
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case sht := <-shutdown:
+		dispatcher.Shutdown()
+		log.Println("main(): Dispatcher is shutting down: ", sht)
+	}
 	//cfg := config.NewConfig()
 	//
 	//dbconn, err := dbase.New(dbase.MYSQL_DSN)
@@ -23,20 +35,20 @@ func main() {
 	//
 	//
 	/*
-	dpt := rabbit.NewDispatcher(cfg.PublishDSN)
-	ctx := context.Background()
-	cmdCreateSensor := rabbit.CreateSensor{
-		CommandModel: rabbit.CommandModel{
-			Id: "w3wq-2da2",
-		},
-		Sensortype: "temp_sensor",
-		Name:       "livingroom01",
-	}
+		dpt := rabbit.NewDispatcher(cfg.PublishDSN)
+		ctx := context.Background()
+		cmdCreateSensor := rabbit.CreateSensor{
+			CommandModel: rabbit.CommandModel{
+				Id: "w3wq-2da2",
+			},
+			Sensortype: "temp_sensor",
+			Name:       "livingroom01",
+		}
 
-	err := dpt.Apply(ctx, cmdCreateSensor)
+		err := dpt.Apply(ctx, cmdCreateSensor)
 
-	if err != nil {
-		fmt.Println("Could Apply Command on Create Sensor")
-	}
-    */
+		if err != nil {
+			fmt.Println("Could Apply Command on Create Sensor")
+		}
+	*/
 }
